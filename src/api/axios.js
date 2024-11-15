a7ef544c57f81406a3c10b4c5f7475c61098aae5
KingJB13 <justin.bais13@gmail.com>
2024-11-15 21:26:36 +0800

    updated packages

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-    //baseURL: 'https://ecarmexss-api-ekgrgmcpcpbjhkgw.southeastasia-01.azurewebsites.net/api/',
-    baseURL: 'http://localhost:5267/api'
+    baseURL: 'https://ecarmexss-api-ekgrgmcpcpbjhkgw.southeastasia-01.azurewebsites.net/api/',
 });
